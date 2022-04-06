9869b9bf44a61255d970d4ce51c37bb4f7dadec2
Yauheni Naskou <evg.noskov.noskov@gmail.com>
2022-04-06 17:35:28 +0300

    added: account module

--- backend/accounting/src/app.ts
+++ backend/accounting/src/app.ts
@@ -2,12 +2,15 @@ import express, { Request, Response } from 'express'
 const app = express()
 import dotenv from 'dotenv'
 import { dataSource } from './config/ormconfig'
+import accountController from './modules/account/account.controller'
 dotenv.config()
 
 app.use(express.static(__dirname))
 app.use(express.json())
 
 const PORT = process.env.PORT || 4000
+
+app.use(`/account`, accountController)
 
 async function start() {
 	await dataSource.initialize()
---
+++ backend/accounting/src/modules/account/account.controller.ts
@@ -0,0 +1,13 @@
+import { Router, Request, Response } from 'express'
+
+const accountController = Router()
+
+accountController.get(`/`, async (req: Request, res: Response) => {
+	try {
+		return res.send(`hi`)
+	} catch (err) {
+		return res.status(400).send(`unknown error`)
+	}
+})
+
+export = accountController
